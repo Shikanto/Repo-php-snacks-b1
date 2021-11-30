5a97a657a480c2347f60c47f240bce662fdb2ba9
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-11-30 10:51:18 +0100

    correction and wrote florian's solution

--- snack1.php
+++ snack1.php
@@ -1,4 +1,14 @@
 <?php
+
+    $name = "";
+    $age = "";
+    $email = "";
+
+    if(!isset($_GET["name"]) || !isset($_GET["age"]) || !isset($_GET["email"])) {
+      exit("Dati mancanti");
+    }
+
+
     $name = $_GET['name'];
     $lenName = strlen($name);
     
@@ -7,10 +17,57 @@
     $age = $_GET['age'];
     
     if ((($lenName >= 3) && (strstr($email, '@') && strstr($email, '.')) && (is_numeric($age)))) {
-        echo "Accesso riuscito";
+        exit ("Accesso riuscito");
       } else {
-        echo "Accesso negato";
+        exit ("Accesso negato");
       }
      
+    /* FLORIAN SOLUZIONE
 
+    controllo la chiave NAME
+
+    if (!isset($_GET["name"]) || strlen($_GET["name"]) <3) {
+      echo "<h1>Accesso Negato</h1>"
+      exit("Valore di 'name' non valido");
+    }
+
+
+    controllo la chiave AGE
+
+    if (!isset($_GET["age"]) || is_numeric($_GET["age"])) {
+      echo "<h1>Accesso Negato</h1>"
+      exit("Valore di 'age' non valido");
+    }
+
+
+    controllo la chiave MAIL
+    if (isset($_GET["mail"])) {
+      $hasAt = strpos ($_GET["mail"], "@");
+
+      if ($hasAt === false) {
+        echo "<h1>Accesso Negato</h1>"
+        exit("Valore di 'mail' non valido");
+      }
+
+       PRENDIAMO SOLO LA PARTE DOPO LA @
+
+      $domain = explode("@", $_GET["mail"])[1];  // qui andiamo a prendere l'indice 1 cioè quello dopo la chiocciola
+
+      if(strpos($domain, ".") === false) {
+        echo "<h1>Accesso Negato</h1>"
+        exit("Valore di 'mail' non valido");
+      }
+    } else {
+      echo "<h1>Accesso Negato</h1>";
+      exit("Valore di 'mail' non valido");
+
+    }
+
+    $name = $_GET["name"];
+    $age = $_GET["age"];
+    $mail = $_GET["mail"];
+
+    echo "<h1>Accesso Riuscito</h1>";
+
+    */
 ?>
